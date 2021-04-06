31a500dee9de82fa41a5ad580efec15a6f6248f8
bhimeshagrawal <bhimeshagrawalggc@gmail.com>
2021-04-06 23:24:44 +0530

    coverts locastorage to firestore

--- index.js
+++ index.js
@@ -1,95 +1,96 @@
 console.log("welcome to notera");
-showNotes();
+// showNotes();
 // If user add note add it to local storage
-var addBtn = document.getElementById("addBtn");
+// var addBtn = document.getElementById("addBtn");
 // when user clicks save button
-addBtn.addEventListener("click", function (e) 
-{
-  //takes content out of text area and store them in variable
-   var addTxt = document.getElementById("notecontent");
-   var addTitle = document.getElementById("notetitle");
-   var notes = localStorage.getItem("savedNotes");
-   //makes a array of objects if notes are zero
-   if (notes == null) 
-   {
-    notesObj = [];
-   } 
-   // if notes are not zero 
-   else 
-   {
-    notesObj = JSON.parse(notes);
-   }
-   let myObj = {
-    title: addTitle.value,
-    text: addTxt.value
-  }
-  //pushing input to local storage
-  notesObj.push(myObj);
-  localStorage.setItem("savedNotes", JSON.stringify(notesObj));
-  //remove previous note inputs
-  addTxt.value = "";
-  addTitle.value = "";
-  // console.log(notesObj);
-  window.location.reload();
-  //input will reflect to note list
-  showNotes();
-});
+// addBtn.addEventListener("click", function (e) 
+// {
+//   //takes content out of text area and store them in variable
+//    var addTxt = document.getElementById("notecontent");
+//    var addTitle = document.getElementById("notetitle");
+//    var notes = localStorage.getItem("savedNotes");
+//    //makes a array of objects if notes are zero
+//    if (notes == null) 
+//    {
+//     notesObj = [];
+//    } 
+//    // if notes are not zero 
+//    else 
+//    {
+//     notesObj = JSON.parse(notes);
+//    }
+   
+//    let myObj = {
+//     title: addTitle.value,
+//     text: addTxt.value
+//   }
+//   //pushing input to local storage
+//   notesObj.push(myObj);
+//   localStorage.setItem("savedNotes", JSON.stringify(notesObj));
+//   //remove previous note inputs
+//   addTxt.value = "";
+//   addTitle.value = "";
+//   // console.log(notesObj);
+//   window.location.reload();
+//   //input will reflect to note list
+//   showNotes();
+// });
 // function to show elements from local storage
-function showNotes() 
-{
-  var notes = localStorage.getItem("savedNotes");
-  if (notes == null) 
-  {
-    notesObj = [];
-  } 
-  else 
-  {
-    notesObj = JSON.parse(notes);
-  }
-  var html = "";
-  notesObj.forEach(function (element, index) 
-  {
-    html += `
-          <div class="notelistitem border">
-          <div class="d-flex">
-          <button class="my-1 btn btn-default deleteBtn " type="submit" id="${index}" onclick="deleteNote(this.id)">
-            <i class="fas fa-trash"></i>
-          </button>
-          <div id="note${index}" class="noteCardWritten" onclick="grabNotes(this.id.substr(id.length - 1))">
-          <div class="notelisttitle" id="title${index}">${element.title}</div>
-          <div class="notelisttext" id="text${index}">${element.text} </div>
-          </div>
-          <button class="my-1 btn btn-default deleteBtn " type="submit" id="e${index}" onclick="editNote(this.id.substr(id.length - 1))">
-            <i class="fas fa-edit"></i>
-          </button>
-          </div>
-          </div>
-          `;
-    var notesElm = document.getElementById("notes");
-    if (notes.length != 0)
-    {
-      notesElm.innerHTML = html;
-    }else{
-      showNotes();
-    }
-  });
-}
-//function to delete notes
-function deleteNote(index) 
-{
-  var notes = localStorage.getItem("savedNotes");
-  if (notes == null) 
-  {
-    notesObj = [];
-  } 
-  else 
-  {
-    notesObj = JSON.parse(notes);
-  }
-  notesObj.splice(index, 1);
-  localStorage.setItem("savedNotes", JSON.stringify(notesObj));
-  showNotes();
-}
+// function showNotes() 
+// {
+//   var notes = localStorage.getItem("savedNotes");
+//   if (notes == null) 
+//   {
+//     notesObj = [];
+//   } 
+//   else 
+//   {
+//     notesObj = JSON.parse(notes);
+//   }
+//   var html = "";
+//   notesObj.forEach(function (element, index) 
+//   {
+//     html += `
+//           <div class="notelistitem border">
+//           <div class="d-flex">
+//           <button class="my-1 btn btn-default deleteBtn " type="submit" id="${index}" onclick="deleteNote(this.id)">
+//             <i class="fas fa-trash"></i>
+//           </button>
+//           <div id="note${index}" class="noteCardWritten" onclick="grabNotes(this.id.substr(id.length - 1))">
+//           <div class="notelisttitle" id="title${index}">${element.title}</div>
+//           <div class="notelisttext" id="text${index}">${element.text} </div>
+//           </div>
+//           <button class="my-1 btn btn-default deleteBtn " type="submit" id="e${index}" onclick="editNote(this.id.substr(id.length - 1))">
+//             <i class="fas fa-edit"></i>
+//           </button>
+//           </div>
+//           </div>
+//           `;
+//     var notesElm = document.getElementById("notes");
+//     if (notes.length != 0)
+//     {
+//       notesElm.innerHTML = html;
+//     }else{
+//       showNotes();
+//     }
+//   });
+// }
+// function to delete notes
+// function deleteNote(index) 
+// {
+//   var notes = localStorage.getItem("savedNotes");
+//   if (notes == null) 
+//   {
+//     notesObj = [];
+//   } 
+//   else 
+//   {
+//     notesObj = JSON.parse(notes);
+//   }
+//   notesObj.splice(index, 1);
+//   localStorage.setItem("savedNotes", JSON.stringify(notesObj));
+//   showNotes();
+// }
 
 let search = document.getElementById("searchTxt");
 search.addEventListener("input", function () 
--- signin.js
+++ signin.js
@@ -1,3 +1,106 @@
+$("#loginBtn").show();
+$("#logoutBtn").hide();
+$('#profileBtn').hide();
+var n;
+var addTitle = document.getElementById("notetitle");
+var addTxt = document.getElementById("notecontent");
+const accountdetails = document.querySelector('.profileModalBody');
+var user = firebase.auth().currentUser;
+var addBtn = document.getElementById("addBtn");
+
+//listener for auth state changes
+auth.onAuthStateChanged(user => {
+    console.log(user);
+    console.log(user.email);
+    // console.log(currentUserEmail);
+    if (user) {
+        db.collection(user.email).get()
+            .then(snapshot => {
+                console.log(snapshot.size)
+                n = snapshot.size
+                console.log(n);
+            });
+        setui(user);
+        addBtn.addEventListener("click", function (e) {
+            console.log(user.email, addTitle.value, addTxt.value);
+            db.collection(user.email).doc("note" + (n + 1)).set({
+                title: addTitle.value,
+                text: addTxt.value
+            })
+                alert("Successfully added, Reload to see changes.");
+        });
+        const setupNotes = (data) => {
+            var index = n;
+            if (data.length) {
+                let html = '';
+                data.forEach(doc => {
+                    const note = doc.data();
+                    const div =
+                        `
+              <div class="notelistitem border">
+                <div class="d-flex">
+                  <button class="my-1 btn btn-default deleteBtn " type="submit" id="${index}" onclick="deleteNote(this.id)"><i class="fas fa-trash"></i></button>
+                  <div id="note${index}" class="noteCardWritten" onclick="grabNotes(this.id.substr(id.length - 1))">
+                    <div class="notelisttitle" id="title${index}">${note.title}</div>
+                    <div class="notelisttext" id="text${index}">${note.text} </div>
+                  </div>
+                  <button class="my-1 btn btn-default deleteBtn " type="submit" id="e${index}" onclick="editNote(this.id.substr(id.length - 1))"><i class="fas fa-edit"></i></button>
+                </div>
+              </div> 
+              `;
+                    html += div;
+                    index++;
+                });
+                noteList.innerHTML = html;
+            } else {
+                noteList.innerHTML =
+                    ` 
+            <div class="container-fluid text-center py-2 my-2">
+            Empty here
+            </div>
+            `
+            }
+        }
+        db.collection(user.email).get().then(snapshot => {
+            setupNotes(snapshot.docs);
+        });
+        
+
+
+    }
+    else {
+        setui();
+    }
+});
+const noteList = document.querySelector(".notelist");
+noteList.innerHTML =
+    ` 
+<div class="container-fluid text-center py-2 my-2">
+Loading ...
+</div>
+`;
+
+//change on user login logout
+const setui = (user) => {
+    if (user) {
+        db.collection('users').doc(user.uid).get().then(doc => {
+            const html = `<div>Logged in as ${doc.data().name} </div><div>${user.email}</div>`;
+            accountdetails.innerHTML = html;
+        });
+        $("#loginBtn").hide();
+        $("#logoutBtn").show();
+        $('#profileBtn').show();
+    } else {
+        accountdetails.innerHTML = '';
+        $("#loginBtn").show();
+        $("#logoutBtn").hide();
+        $('#profileBtn').hide();
+    }
+}
+
+
+
+//sign up
 const registerForm = document.querySelector('#registerForm');
 registerForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -6,10 +109,17 @@ registerForm.addEventListener('submit', (e) => {
     var password = registerForm['userRegisterPassword'].value;
     console.log(email, password);
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred.user);
+        $("#closeModal").click();
+        return db.collection('users').doc(cred.user.uid).set({
+            name: registerForm['userRegisterName'].value,
+        });
+        
         registerForm.reset();
     });
 });
+
+
+//sign in 
 const loginForm = document.querySelector('#loginForm');
 loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -18,7 +128,20 @@ loginForm.addEventListener('submit', (e) => {
     var password = loginForm['userLoginPassword'].value;
     console.log(email, password);
     auth.signInWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred.user);
+        // console.log(cred.user);
+        $("#closeModal").click();
         loginForm.reset();
     });
 });
+
+
+//logout
+const logout = document.querySelector('#logoutBtn');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        console.log("user logged out");
+        // $("#closeModal").click();
+        location.reload();
+    })
+})
