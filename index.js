a808369020f766d707e4056ef5c87a7ab0dca313
bhimeshagrawal <bhimeshagrawalggc@gmail.com>
2021-04-08 15:06:36 +0530

    adds feature to delete and edit notes

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ console.log("welcome to notera");
 //    {
 //     notesObj = JSON.parse(notes);
 //    }
-   
+
 //    let myObj = {
 //     title: addTitle.value,
 //     text: addTxt.value
@@ -92,74 +92,93 @@ console.log("welcome to notera");
 //   showNotes();
 // }
 
+
+
+
+
 let search = document.getElementById("searchTxt");
-search.addEventListener("input", function () 
-{
+search.addEventListener("input", function () {
   let inputVal = search.value;
   let notelistitems = document.getElementsByClassName("notelistitem");
-  Array.from(notelistitems).forEach(function (element) 
-  {
+  Array.from(notelistitems).forEach(function (element) {
     let cardTxt = element.getElementsByClassName("notelisttext")[0].innerText;
-    if (cardTxt.includes(inputVal)) 
-    {
+    if (cardTxt.includes(inputVal)) {
       element.style.display = "block";
-    } 
-    else 
-    {
+    }
+    else {
       element.style.display = "none";
     }
   });
 });
 //when click on any note in list then this function runs
-function grabNotes(index){
+function grabNotes(index) {
   parent = document.getElementById("note" + index)
-  childrenTitle =  parent.children[0];
+  childrenTitle = parent.children[0];
   childrenText = parent.children[1];
   document.getElementById("disableViewBtn").classList.remove("disabled");
   // console.log(` text${index} is title `+ childrenTitle.innerHTML);
   // console.log(` text${index} is text `+ childrenText.innerHTML);
   $("#notetitle").html(childrenTitle.innerHTML);
   $("#notecontent").html(childrenText.innerHTML);
-  $("#disableViewBtn").css("display","list-item");
-  $("#addBtn").css("display","none");
-  $("#notetitle").prop("disabled","true");
-  $("#notecontent").prop("disabled","true");
+  $("#disableViewBtn").css("display", "list-item");
+  $("#addBtn").css("display", "none");
+  $("#notetitle").prop("disabled", "true");
+  $("#notecontent").prop("disabled", "true");
 }
 //when we click cross button this runs
-function disableView(){
+function disableView() {
   $("#notetitle").html("");
   $("#notecontent").html("");
-  $("#disableViewBtn").css("display","none");
-  $("#addBtn").css("display","list-item");
+  $("#disableViewBtn").css("display", "none");
+  $("#addBtn").css("display", "list-item");
   $("#notetitle").removeAttr("disabled");
   $("#notecontent").removeAttr("disabled");
-  window.location.reload();
+  // window.location.reload();
 }
-function editNote(index){
+function deleteNote(b) {
+  if (firebase.auth().currentUser !== null) {
+    // console.log("user id: " + firebase.auth().currentUser.email);
+    var t = firebase.auth().currentUser.email;
+    db.collection(t).doc(b).delete().then(() => {
+
+      var l = "notecard" + b;
+      document.getElementById(l).style.display = "none";
+      // alert("Reload to see changes");
+      // console.log('Document successfully deleted!');
+    }).catch((error) => {
+      console.error('Error removing document: ', error);
+    });
+  }
+
+}
+
+function editNote(index) {
+  console.log(index)
   parent = document.getElementById("note" + index)
-  childrenTitle =  parent.children[0];
+  childrenTitle = parent.children[0];
   childrenText = parent.children[1];
   document.getElementById("disableViewBtn").classList.remove("disabled");
   $("#notetitle").html(childrenTitle.innerHTML);
   $("#notecontent").html(childrenText.innerHTML);
   $("#notetitle").removeAttr("disabled");
   $("#notecontent").removeAttr("disabled");
+
   deleteNote(index);
 }
 
-function toggleResetPswd(e){
+function toggleResetPswd(e) {
   e.preventDefault();
   $('#logreg-forms .form-signin').toggle() // display:block or none
   $('#logreg-forms .form-reset').toggle() // display:block or none
 }
 
-function toggleSignUp(e){
+function toggleSignUp(e) {
   e.preventDefault();
   $('#logreg-forms .form-signin').toggle(); // display:block or none
   $('#logreg-forms .form-signup').toggle(); // display:block or none
 }
 
-$(()=>{
+$(() => {
   // Login Register Form
   $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
   $('#logreg-forms #cancel_reset').click(toggleResetPswd);
--- signin.js
+++ signin.js
@@ -10,41 +10,47 @@ var addBtn = document.getElementById("addBtn");
 
 //listener for auth state changes
 auth.onAuthStateChanged(user => {
-    console.log(user);
-    console.log(user.email);
+    // console.log(user);
+    t = user.email ;
+    
+    // console.log(user.email);
+  
     // console.log(currentUserEmail);
     if (user) {
         db.collection(user.email).get()
             .then(snapshot => {
-                console.log(snapshot.size)
-                n = snapshot.size
-                console.log(n);
+                // console.log(snapshot.size)
+                // n = snapshot.size
+                // console.log(n);
             });
         setui(user);
         addBtn.addEventListener("click", function (e) {
-            console.log(user.email, addTitle.value, addTxt.value);
-            db.collection(user.email).doc("note" + (n + 1)).set({
+            // console.log(user.email, addTitle.value, addTxt.value);
+            db.collection(user.email).doc(addTitle.value).set({
                 title: addTitle.value,
                 text: addTxt.value
             })
                 alert("Successfully added, Reload to see changes.");
+                addTitle.value = "";
+                addTxt.value = "";
         });
+        
         const setupNotes = (data) => {
-            var index = n;
+            var index = 0
             if (data.length) {
                 let html = '';
                 data.forEach(doc => {
                     const note = doc.data();
                     const div =
                         `
-              <div class="notelistitem border">
-                <div class="d-flex">
-                  <button class="my-1 btn btn-default deleteBtn " type="submit" id="${index}" onclick="deleteNote(this.id)"><i class="fas fa-trash"></i></button>
-                  <div id="note${index}" class="noteCardWritten" onclick="grabNotes(this.id.substr(id.length - 1))">
+              <div class="notelistitem border" id="notecard${note.title}">
+                <div class="d-flex" >
+                  <button class="my-1 btn btn-default deleteBtn " type="submit" id="${note.title}" onclick="deleteNote(this.id)"><i class="fas fa-trash"></i></button>
+                  <div id="note${note.title}" class="noteCardWritten" onclick="grabNotes(this.id.substr(4))">
                     <div class="notelisttitle" id="title${index}">${note.title}</div>
                     <div class="notelisttext" id="text${index}">${note.text} </div>
                   </div>
-                  <button class="my-1 btn btn-default deleteBtn " type="submit" id="e${index}" onclick="editNote(this.id.substr(id.length - 1))"><i class="fas fa-edit"></i></button>
+                  <button class="my-1 btn btn-default deleteBtn " type="submit" id="e${note.title}" onclick="editNote(this.id.substr(1))"><i class="fas fa-edit"></i></button>
                 </div>
               </div> 
               `;
@@ -64,8 +70,7 @@ auth.onAuthStateChanged(user => {
         db.collection(user.email).get().then(snapshot => {
             setupNotes(snapshot.docs);
         });
-        
-
+       
 
     }
     else {
@@ -76,7 +81,7 @@ const noteList = document.querySelector(".notelist");
 noteList.innerHTML =
     ` 
 <div class="container-fluid text-center py-2 my-2">
-Loading ...
+Some free space here ...
 </div>
 `;
 
@@ -84,12 +89,16 @@ Loading ...
 const setui = (user) => {
     if (user) {
         db.collection('users').doc(user.uid).get().then(doc => {
-            const html = `<div>Logged in as ${doc.data().name} </div><div>${user.email}</div>`;
-            accountdetails.innerHTML = html;
+            // const html = `<div>Logged in as ${doc.data().name} </div><div>${user.email}</div>`;
+            // accountdetails.innerHTML = html;
+            $(".profileName").html(doc.data().name)
+            $(".profileEmail").html(user.email)
         });
         $("#loginBtn").hide();
         $("#logoutBtn").show();
         $('#profileBtn').show();
+        
+        
     } else {
         accountdetails.innerHTML = '';
         $("#loginBtn").show();
@@ -107,7 +116,7 @@ registerForm.addEventListener('submit', (e) => {
     //get user info
     var email = registerForm['userRegisterEmail'].value;
     var password = registerForm['userRegisterPassword'].value;
-    console.log(email, password);
+    // console.log(email, password);
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
         $("#closeModal").click();
         return db.collection('users').doc(cred.user.uid).set({
@@ -126,7 +135,7 @@ loginForm.addEventListener('submit', (e) => {
     //get user info
     var email = loginForm['userLoginEmail'].value;
     var password = loginForm['userLoginPassword'].value;
-    console.log(email, password);
+    // console.log(email, password);
     auth.signInWithEmailAndPassword(email, password).then(cred => {
         // console.log(cred.user);
         $("#closeModal").click();
@@ -140,7 +149,7 @@ const logout = document.querySelector('#logoutBtn');
 logout.addEventListener('click', (e) => {
     e.preventDefault();
     auth.signOut().then(() => {
-        console.log("user logged out");
+        // console.log("user logged out");
         // $("#closeModal").click();
         location.reload();
     })
